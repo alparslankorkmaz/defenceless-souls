5aad88d41f5be9c2bc8b1459444db1938acb3f84
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-07-22 17:42:06 +0100

    adjust header and hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,19 @@ export default function Header() {
   };
   return (
     <>
-      <header className=" py-5 flex justify-between items-center">
+      <header className=" py-5 grid grid-cols-8 items-center">
         <img
           src="/images/ds-logo.jpg"
           alt="Defenceless Souls Logo"
-          className="w-48 h-auto mx-auto md:mx-0 rounded-full border-2 border-ds-light-green"
+          className="col-span-2 col-start-4 md:col-span-1 h-auto mx-auto md:mx-0 rounded-full border-2 border-ds-light-green"
         />
-        <h1 className="font-bold text-2xl md:text-4xl drop-shadow-lg text-ds-dark-green">
+        <h1 className="text-center col-span-4 col-start-3 md:col-span-3 flex items-center font-bold text-3xl md:text-4xl drop-shadow-lg text-ds-dark-green">
           Defenceless Souls Dog Sanctuary
         </h1>
+
         <button
           onClick={scroll}
-          className="hidden p-3 text-2xl tracking-wider rounded-3xl bg-hero-pattern bg-cover shadow-xl border-4 uppercase text-white border-ds-light-green  font-semibold md:flex flex-col items-center justify-center hover:scale-105"
+          className="h-14 col-start-7 col-span-2 md:text-xl hidden text-2xl tracking-wide rounded-3xl bg-hero-pattern bg-cover shadow-xl border-4 uppercase text-white border-ds-light-green font-semibold md:flex items-center justify-center hover:scale-105"
         >
           volunteer form
         </button>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
       <div
         data-aos="fade-down"
         data-aos-delay="300"
-        className="hero grid grid-cols-6 gap-10 items-center my-20 shadow-2xl p-10 rounded bg-gradient-to-br from-ds-beige to-slate-300"
+        className="hero grid grid-cols-6 gap-10 my-20 shadow-2xl p-10 rounded bg-gradient-to-br from-ds-beige to-slate-300"
       >
         <div className="hero-text col-span-6 md:col-span-3 mt-11 order-last md:order-first text-center md:text-start">
           <h2 className="uppercase text-xl md:text-4xl font-bold tracking-wide pb-5 bg-hero-pattern bg-cover bg-clip-text ">
@@ -18,10 +18,13 @@ export default function Hero() {
             </span>
           </h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat,
-            quas quo ut a officiis voluptate corrupti repudiandae incidunt,
-            error reprehenderit laboriosam omnis sapiente culpa sed optio maxime
-            nobis animi ab.
+            At our beloved dog shelter in Gölyazı, we strive to provide a safe
+            and loving environment for abandoned and neglected stray dogs.
+            However, without secure fences, our furry friends are constantly
+            exposed to the dangers of the nearby road. We urgently need your
+            assistance to build sturdy fences around our shelter, ensuring the
+            safety and well-being of these defenceless souls. Each month we lose
+            at least two dogs to a road accident.{" "}
           </p>
         </div>
         <img
@@ -31,7 +34,7 @@ export default function Hero() {
         />
         <button
           onClick={scroll}
-          className="md:hidden  col-span-6 order-last md:order-first mt-10 p-6 max-h-4 text-xl md:text-3xl tracking-wider bg-hero-pattern bg-cover border-4 border-ds-light-green rounded-xl uppercase text-white font-semibold flex flex-col items-center justify-center hover:scale-105"
+          className="md:hidden col-span-4 col-start-2 order-last md:order-first mt-10 p-6 max-h-4 text-xl md:text-3xl tracking-wider bg-hero-pattern bg-cover border-4 border-ds-light-green rounded-xl uppercase text-white font-semibold flex flex-col items-center justify-center hover:scale-105"
         >
           volunteer form
         </button>
